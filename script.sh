27b4b15e11da90c1f21812be2b6b327e7f4dc59e
Alexis_Solis <alexissolis019@gmail.com>
2024-10-27 21:45:38 +0100

    Vérification du nombre de paramètres, récuperation du parametres ville, sauvegarde des donées brutes dans un fichier local

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
-#ceci est le script
+#! /bin/sh
+
+#verification nombre de parametres
+if [ $# -ne 1 ]
+then
+	echo "Usage : ./script.sh <ville>"
+	exit 1
+fi
+
+#récuperation parametres ville 
+ville=$1
+
+#exportation données brutes dans fichier.txt
+curl -s wttr.in/$ville?format=j2 > local.txt
+
+exit 0
