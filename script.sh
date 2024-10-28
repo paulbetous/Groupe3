97eb172247baaba212fa559c2fd9ebbb71e45983
faurite <faurite.natan@gmail.com>
2024-10-28 14:57:21 +0100

    Ajout de la possibilité de récupérer la température actuelle et mettre la température du lendemain dans un fichier local

--- script.sh
+++ script.sh
@@ -13,4 +13,13 @@ ville=$1
 #exportation données brutes dans fichier.txt
 curl -s wttr.in/$ville?format=j2 > local.txt
 
-exit 0
+#récupération température actuelle
+tempact=$(curl -s wttr.in/$ville?format="%t")
+if [ "${tempact:0:1}" = "+" ]
+then
+    tempact=$(echo $tempact | cut -c 2-)
+fi
+
+#récupération température du lendemain
+templen=$(head -n 103 local.txt | tail -n 1 | grep -oE "\-*[0-9]*")°C
+exit 0 
